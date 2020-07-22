1093198a2a1891ed27cefeea3707316f72a6265b
Bryan Kottama <bryan@kyodo-i.com>
2020-07-22 20:22:55 +0700

    change layout

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,82 +1,124 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('/assets/fontawesome/css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/assets/css/admin.css') }}">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>@yield('title')</title>
-  </head>
-  <body>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{ asset('/assets/fontawesome/css/all.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Admin | <b>NAMA TOKO</b></a>
-      <form class="form-inline my-2 my-lg-0 ml-auto">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
-      <div class="icon ml-4">
-        <h5>
-          <i class="fas fa-envelope mr-3" data-toggle="tooltip" title="Pesan Masuk"></i>
-          <i class="fas fa-bell mr-3" data-toggle="tooltip" title="Notifikasi"></i>
-          <i class="fas fa-sign-out-alt mr-3" data-toggle="tooltip" title="Log Out"></i>
-        </h5>
+  <title>@yield('title')</title>
+</head>
+
+<body>
+  <div class="wrapper">
+    <!-- Sidebar  -->
+    <nav id="sidebar">
+      <div class="sidebar-header">
+        <h3>Nama Toko</h3>
+        <button type="button" id="sidebarCollapse" class="btn btn-info">
+          <i class="fas fa-align-left"></i>
+        </button>
       </div>
-      </div>
+
+      <ul class="list-unstyled components">
+        <li>
+          <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+            <i class="fas fa-home mr-3"></i>
+            Home
+          </a>
+          <ul class="collapse list-unstyled" id="homeSubmenu">
+            <li>
+              <a href="#">Home 1</a>
+            </li>
+            <li>
+              <a href="#">Home 2</a>
+            </li>
+            <li>
+              <a href="#">Home 3</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fas fa-briefcase mr-3"></i>
+            About
+          </a>
+          <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+            <i class="fas fa-copy mr-3"></i>
+            Pages
+          </a>
+          <ul class="collapse list-unstyled" id="pageSubmenu">
+            <li>
+              <a href="#">Page 1</a>
+            </li>
+            <li>
+              <a href="#">Page 2</a>
+            </li>
+            <li>
+              <a href="#">Page 3</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+
     </nav>
 
-    <div class="row no-gutters">
-      <div class="col-md-2 bg-dark pr-3 pt-4">
+    <!-- Page Content  -->
+    <div id="content">
+      {{-- @yield('container') --}}
+    </div>
+  </div>
+
+  {{-- <div class="row no-gutters">
+      <div class="col-md-2 bg-light pr-3 pt-4">
         <ul class="nav flex-column ml-3 mb-5">
           <li class="nav-item">
-            <a class="nav-link active text-white" href="/dashboard"><i class="fas fa-tachometer-alt mr-2"></i>Dashboard</a>
-            <hr class="bg-secondary">
+            <a class="navbar-brand" href="#">Admin | <b>NAMA TOKO</b></a>
           </li>
           <li class="nav-item">
-            <a class="dropdown-btn nav-link text-white" href="/product"><i class="fas fa-cubes mr-2"></i>Produk</a>
-            <div class="dropdown dropdown-container">
-              <a class="dropdown-item" href="#">Tambah Produk</a>
-              <a class="dropdown-item" href="#">Edit Produk</a>
-              <a class="dropdown-item" href="#">Link 3</a>
-            </div>
-            <hr class="bg-secondary">
+            <a class="nav-link active" href="/dashboard"><i class="fas fa-tachometer-alt mr-2"></i>Dashboard</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#"><i class="fas fa-shopping-cart mr-2"></i>Pemesanan</a>
-            <hr class="bg-secondary">
+            <a class="dropdown-btn nav-link" href="/product"><i class="fas fa-cubes mr-2"></i>Produk</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#"><i class="fas fa-money-check-alt mr-2"></i>Pembayaran</a>
-            <hr class="bg-secondary">
+            <a class="nav-link" href="#"><i class="fas fa-shopping-cart mr-2"></i>Pemesanan</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#"><i class="fas fa-file-invoice mr-2"></i>Arsip</a>
-            <hr class="bg-secondary">
+            <a class="nav-link" href="#"><i class="fas fa-money-check-alt mr-2"></i>Pembayaran</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fas fa-file-invoice mr-2"></i>Arsip</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fas fa-sign-out-alt mr-2"></i>Logout</a>
           </li>
         </ul>
       </div>
       @yield('container')
-    </div>
-
-    
+    </div> --}}
 
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-      crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-      crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-      integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
-      crossorigin="anonymous"></script>
-    <script type="text/javascript" src="{{ asset('/assets/js/admin.js') }}"></script>
-  </body>
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+  </script>
+  <script type="text/javascript" src="{{ asset('/js/app.js') }}"></script>
+</body>
+
 </html>
